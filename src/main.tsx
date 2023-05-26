5f35eb43851a3aa5b36b617328081cdd01b6bf1a
Danhalf <danhel@rocketmail.com>
2023-05-26 19:12:35 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store.tsx';
+import { Provider } from 'react-redux';
+
+const root = document.getElementById('root') as HTMLElement
+
+createRoot(root).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
